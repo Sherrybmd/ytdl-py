5488cc4902ac31502e8202fe701d64ce38022e2d
Sherrybmd <shahryar.babamohamadi.2006@gmail.com>
2025-12-31 22:48:17 +0330

    fix bug with audio dl commands

--- ytdl.py
+++ ytdl.py
@@ -60,7 +60,7 @@ class linuxytdl:
         self.downVideo(quality, name)
 
     def downAudio(self, name):
-        self.extras += "-f bestaudio --extract-audio --audio-quality 320k --recode mp3"
+        self.extras += "yt-dlp -x --audio-format mp3 "
         self.getOutputName(name, "")
         self.download()
 
